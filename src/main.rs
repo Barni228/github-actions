a425ab122e1d4ee5901838511c2da8013e4d9942
Andrii Pukhalyk <pukhalyk1@gmail.com>
2026-04-24 13:39:50 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use num_traits::AsPrimitive;
+
+#[allow(dead_code)]
+/// Add, which is unnecessarily generic
+fn add<Out>(a: impl AsPrimitive<Out>, b: impl AsPrimitive<Out>) -> Out
+where
+    Out: Copy + 'static + std::ops::Add<Output = Out>,
+{
+    a.as_() + b.as_()
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(add::<usize>(2i32, 2u8), 4);
+    }
+}
